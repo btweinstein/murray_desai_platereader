802c55937e0bc6a1e86d138a9ded4de57e0c98da
btweinstein <btweinstein@gmail.com>
2015-04-23 11:39:15 -0400

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='murray_desai_platereader',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/btweinstein/murray_desai_platereader',
+    license='',
+    author='Bryan Weinstein',
+    author_email='bweinstein@seas.harvard.edu',
+    description='Used by the murray and desai labs at Harvard to analyze plate reader data'
+)
