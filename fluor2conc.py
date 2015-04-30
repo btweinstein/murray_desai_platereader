ea051c176e32806cb8855b93544da1cfe79ccff2
btweinstein <btweinstein@gmail.com>
2015-04-30 18:50:51 -0400

    Perhaps the first working copy of the command line code.

--- fluor2conc.py
+++ fluor2conc.py
@@ -1,6 +1,7 @@
 import murray_desai_platereader as mdp
 import argparse as ap
 import re
+import sys
 
 parser = ap.ArgumentParser(description='Converts plate reader fluorescence measurements into DNA concentrations.')
 parser.add_argument('filename', help='The path to the desired excel file.')
@@ -14,7 +15,6 @@ print parsed_info
 
 # Now parse the inputs
 file_name = parsed_info.filename
-
 table_position = parsed_info.table_position
 if table_position is None:
     table_position = 'A1'
@@ -28,11 +28,26 @@ if high_fluor_position is None:
 
 # Parse the strings
 
-match = re.match(r"([a-z]+)([0-9]+)", 'foofo21', re.I)
-if match:
-    items = match.groups()
+def get_string_then_digits(input_string):
+    match = re.match(r"([a-z]+)([0-9]+)", input_string , re.I)
+    items = None
+    if match:
+        items = match.groups()
+        # Convert second piece to number
+        items[1] = int(items[1])
+    return items
 
-print table_position
 
-#experiment = mdp.PlateReader_Experiment(file_name, table_position, low_fluor_position, high_fluor_position, **kwargs)
-#conc = experiment.get_concentration()
+table_position = get_string_then_digits(table_position)
+
+if len(table_position[0]) > 1:
+    print 'pls organize ur excel file, y r u in AA'
+    sys.exit(1)
+
+low_fluor_position = get_string_then_digits(low_fluor_position)
+high_fluor_position = get_string_then_digits(high_fluor_position)
+
+experiment = mdp.PlateReader_Experiment(file_name, table_position, low_fluor_position, high_fluor_position)
+conc = experiment.get_concentration()
+
+print conc
